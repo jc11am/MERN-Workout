b1040d2889ca528c901e616e279a28ae24545220
jc11am <whoisjoeam@gmail.com>
2022-10-18 21:41:58 +0100

    Created email and password validation

--- backend/model/userSchema.js
+++ backend/model/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
+const validator = require("validator")
 
 const userSchema = mongoose.Schema({
     email: {
@@ -15,6 +16,20 @@ const userSchema = mongoose.Schema({
 })
 
 userSchema.statics.signup = async function( email, password ) {
+
+    //validation
+    if(!email || !password){
+        throw Error ("All fields are required")
+    }
+
+    if(!validator.isEmail(email)){
+        throw Error ("Email is not valid")
+    }
+
+    if(!validator.isStrongPassword(password)){
+        throw Error ("Password not strong enough")
+    }
+
     const exist = await this.findOne( {email} )
 
     if( exist ) {
